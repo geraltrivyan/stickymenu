151c2cb447fd85ed24dbcdeb7af7147ed8b00c26
GneatGeek <gneatgeek@gmail.com>
2012-11-30 15:13:17 -0800

    Commented and improved efficiency of stickyMenu.js

--- stickyMenu.js
+++ stickyMenu.js
@@ -1,29 +1,43 @@
-(function($) {
-	$.fn.stickyMenu = function(cn) {
-		var pinned;
-		var menu  = this;
-		var start = this.offset().top;
-		var id = menu.attr('id');
+/**
+ * Sticky Menu is a super lightweight jQuery plugin to make a menu sticky/dockable/pinable.
+ * @author Rory Hardy [GneatGeek]
+ * @param {Object} $ - jQuery
+ * @param {Object} window
+ */
 
-		if (!cn)
-			var cn = "sticky";
+(function($, window) {
+	/**
+	 * @namespace fn.stickyMenu
+	 * @method stickyMenu
+	 * @param {Object} cname - Class name to append to the object
+	 * @return {Object} this - Maintains Chainability
+	 */
+	$.fn.stickyMenu = function(cname) {
+		var pinned; // Boolean Value to increase efficiency due to window.scroll's frequent execution
+		var menu  = this; // The value of this will change inside the bind function so we have to store it.
+		var start = menu.offset().top; // Initial position of your menu
+		var id    = 'scroll.stickymenu-' + menu.attr('id'); // We need to namespace all functions.
 
-		$(window).bind('scroll.stickymenu-' + id, function() {
+		if (!cname) // Default CSS class to sticky if the user didn't specify their own
+			var cname = "sticky";
+
+		$(window).bind(id, function() {
+			// Test to see if it is not yet pinned and if it meets our positioning requirements to pin.
 			if (!pinned && (menu.offset().top - $(this).scrollTop() < 0)) {
 				menu.css({
 					'top' : 0,
 					'position' : 'fixed'
-				}).toggleClass(cn);
+				}).toggleClass(cname);
 				pinned = true;
 			} else if (pinned && $(this).scrollTop() <= start) {
 				menu.css({
 					'position' : 'absolute',
 					'top' : start + 'px'
-				}).toggleClass(cn);
+				}).toggleClass(cname);
 				pinned = false;
 			}
 		});
 
 		return this;
 	};
-})(jQuery); 
+})(jQuery, window);
