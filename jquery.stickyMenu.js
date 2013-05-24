818cdbb02c2d5862b6e21ea39c12e5d32a66c5de
GneatGeek <gneatgeek@gmail.com>
2013-05-23 23:27:17 -0500

    Cleaned up formatting

--- jquery.stickyMenu.js
+++ jquery.stickyMenu.js
@@ -6,34 +6,34 @@
  */
 
 (function($, window) {
-	/**
-	 * @namespace fn.stickyMenu
-	 * @method stickyMenu
-	 * @param  {Object} cname - Class name to append to the object
-	 * @return {Object} this - Maintains Chainability
-	 */
 
-	$.fn.stickyMenu = function(cname) {
-		var pinned;       // Boolean Value to increase efficiency due to window.scroll's frequent execution
-		var menu  = this; // The value of this will change inside the bind function so we have to store it.
-		var start = menu.offset().top;                      // Initial position of your menu
-		var id    = 'scroll.stickymenu-' + menu.attr('id'); // We need to namespace all functions.
+  /**
+   * @namespace fn.stickyMenu
+   * @method stickyMenu
+   * @param  {Object} cname - Class name to append to the object
+   * @return {Object} this - Maintains Chainability
+   */
+  $.fn.stickyMenu = function(cname) {
+    var pinned,
+        menu  = this,
+        start = menu.offset().top;
 
-		if (!cname) // Default CSS class to sticky if the user didn't specify their own
-			var cname = "sticky";
+    if ( !cname )
+      cname = "sticky";
 
-		$(window).bind(id, function() {
-			if (pinned) {
-				if ($(this).scrollTop() <= start) {
-					menu.toggleClass(cname);
-					pinned = false;
-				}
-			} else if ($(this).scrollTop() > start) {
-				menu.toggleClass(cname);
-				pinned = true;
-			}
-		});
+    $(window).bind('scroll.stickymenu-' + menu.attr('id'), function() {
+      if (pinned) {
+        if ( $(this).scrollTop() <= start ) {
+          menu.toggleClass(cname);
+          pinned = false;
+        }
+      } else if ( $(this).scrollTop() > start ) {
+        menu.toggleClass(cname);
+        pinned = true;
+      }
+    });
 
-		return this;
-	};
-})(jQuery, window); 
+    return this;
+  };
+})(jQuery, window);
+
